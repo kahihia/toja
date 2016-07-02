d06b0d151566c7e346befb0456e0a42af18532f3
Phong Nguyen <xphongvn@gmail.com>
2016-07-02 14:39:21 +0900

    quick fix

--- server/reservation/views.py
+++ server/reservation/views.py
@@ -14,7 +14,7 @@ def reservation(request):
 
     if request.method == "POST":
         xml = '<Response> ' \
-                '<Gather timeout="20" finishOnKey="" numDigits=1 method="GET" action="http://47.88.212.198:8000/gather/"> ' \
+                '<Gather timeout="20" finishOnKey="" numDigits="1" method="GET" action="http://47.88.212.198:8000/gather/"> ' \
                     '<Say language="en-US"> Hi, this is an automated call from Toja. We want to reserve a table for two people at 7PM today. ' \
                                             'Please press one to accept the reservation, press zero to decline! Or press 5 to listen to the message again. ' \
                                             'Please finish with the star key. ' \
@@ -46,7 +46,7 @@ def gather(request):
 
     elif digit == '5':
         xml = '<Response> ' \
-              '<Gather timeout="20" finishOnKey="" numDigits=1 method="GET" action="http://47.88.212.198:8000/gather/"> ' \
+              '<Gather timeout="20" finishOnKey="" numDigits="1" method="GET" action="http://47.88.212.198:8000/gather/"> ' \
               '<Say language="en-US"> Hi, this is an automated call from Toja. We want to reserve a table for two people at 7PM today. ' \
               'Please press one to accept the reservation, press zero to decline! Or press 5 to listen to the message again. ' \
               'Please finish with the star key. ' \
@@ -57,7 +57,7 @@ def gather(request):
 
     else:
         xml = '<Response> ' \
-              '<Gather timeout="20" finishOnKey="" numDigits=1 method="GET" action="http://47.88.212.198:8000/gather/"> ' \
+              '<Gather timeout="20" finishOnKey="" numDigits="1" method="GET" action="http://47.88.212.198:8000/gather/"> ' \
               '<Say language="en-US"> Sorry. You chose the wrong number! Please choose again. Number one to accept the reservation. ' \
               'Zero to decline the reservation. Or number 5 to listen to the reservation information again.' \
               '</Say> ' \
