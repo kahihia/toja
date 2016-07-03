873526dd4085ed386356df5d325e14c75c895f3c
Phong Nguyen <xphongvn@gmail.com>
2016-07-03 14:33:49 +0900

    Refine call status.

--- server/reservation/views.py
+++ server/reservation/views.py
@@ -293,6 +293,8 @@ def get_twilio_call_status(request, pk):
         call_info.status = Call.FAILED
     elif status in ['ringing', 'in-progress']:
         call_info.status = Call.ON_CALLING
+    else:
+        call_info.status = Call.READY
     call_info.save()
     serializer = CallSerializer(call_info)
     return JSONResponse(serializer.data)
